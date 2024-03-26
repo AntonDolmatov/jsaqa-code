c5c62c4270e40d98ddc7d6deefcd54e3abd499c7
Anton Dolmatov <tosha.dolmatow@yandex.ru>
2024-03-26 15:53:00 +0300

    add test

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -1,17 +1,24 @@
 const sorting = require("../../app");
 
 describe("Books names test suit", () => {
-  it("Books names should be sorted in ascending order", () => {
-    expect(
-      sorting.sortByName([
-        "Гарри Поттер",
-        "Властелин Колец",
-        "Волшебник изумрудного города",
-      ])
-    ).toEqual([
-      "Властелин Колец",
-      "Волшебник изумрудного города",
-      "Гарри Поттер",
-    ]);
-  });
+	test("Books names should be sorted in ascending order", () => {
+		expect(
+			sorting.sortByName([
+				"Гарри Поттер",
+				"Властелин Колец",
+				"Волшебник изумрудного города",
+			])
+		).toEqual([
+			"Властелин Колец",
+			"Волшебник изумрудного города",
+			"Гарри Поттер",
+		]);
+	});
+
+	test("No sorting", () => {
+		expect(
+			sorting.sortByName(["Гарри Поттер", "Властелин Колец", "Гарри Поттер"])
+		).toEqual(["Властелин Колец", "Гарри Поттер", "Гарри Поттер"]);
+	});
+
 });
